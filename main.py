68682ab0836b0cc810bc8a424204b72ceb40f794
SX-7 <sn1.test.preria.2002@gmail.com>
2023-02-26 23:34:45 +0100

    Added gif support

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import requests
 from PIL import Image
 from PIL import ImageDraw
 from PIL import UnidentifiedImageError
+from PIL import ImageSequence
 try:
-    source_image = Image.open(BytesIO(requests.get(sys.argv[1]).content))
+    source_image = Image.open(BytesIO(requests.get("https://media.tenor.com/wKNwPQiMz9EAAAAd/wtff-cat-confused-orange.gif").content))
 except IndexError:
     print("URL adress has not been provided")
     sys.exit(-1)
@@ -17,15 +18,21 @@ except:
     sys.exit(-3)
 #additional handling for animated content?
 
-#higher level overview - make a speechbubble from pieslice and ellipse, but inverted - it's a mask for tranparency layer
-bubble_mask = Image.new("RGBA",(1000,1000),(255,255,255,255))
-bubble_mask_drawable = ImageDraw.Draw(bubble_mask)
-#in finalized version this will probably need custom inputs
-bubble_mask_drawable.pieslice(xy=(0,0,1000,750),start=270,end=280,fill=(255,255,255,0))
-bubble_mask_drawable.ellipse(xy=(-500,-1000,1500,150),fill=(255,255,255,0))
-bubble_mask = bubble_mask.resize((source_image.width,source_image.height),resample=Image.Resampling.NEAREST)
-transparent = Image.new("RGBA",(source_image.width,source_image.height),(255,255,255,0))
-result = Image.composite(source_image,transparent,bubble_mask)
+output_frames =[]
+frame_length=[]
+
+for frame in ImageSequence.Iterator(source_image):
+    #higher level overview - make a speechbubble from pieslice and ellipse, but inverted - it's a mask for tranparency layer
+    
+    bubble_mask = Image.new("RGBA",(1000,1000),(255,255,255,255))
+    bubble_mask_drawable = ImageDraw.Draw(bubble_mask)
+    #in finalized version this will probably need custom inputs
+    bubble_mask_drawable.pieslice(xy=(0,0,1000,750),start=270,end=280,fill=(255,255,255,0))
+    bubble_mask_drawable.ellipse(xy=(-500,-1000,1500,150),fill=(255,255,255,0))
+    bubble_mask = bubble_mask.resize((source_image.width,source_image.height),resample=Image.Resampling.NEAREST)
+    transparent = Image.new("RGBA",(source_image.width,source_image.height),(255,255,255,0))
+    output_frames.append(Image.composite(frame,transparent,bubble_mask))
+    frame_length.append(frame.info['duration'])
 
 #how to handle output? termux's an issue
-result.save("res.gif")
+output_frames[0].save("res.gif",save_all=True, append_images=output_frames[1:],loop=0,duration=frame_length)
